d2c58ff90fe7f0d1e2ffb33b5365273cbafb3488
Dmitriy Okhrimenko <dm.okhrimenko@gmail.com>
2020-03-19 21:51:28 +0200

    change app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log(123);
+// подключение express
+const express = require("express");
+// создаем объект приложения
+const app = express();
+// определяем обработчик для маршрута "/"
+app.get("/", function(request, response){
+     
+    // отправляем ответ
+    response.send("<h2>Привет Express!</h2>");
+});
+// начинаем прослушивать подключения на 3000 порту
+app.listen(3000);
